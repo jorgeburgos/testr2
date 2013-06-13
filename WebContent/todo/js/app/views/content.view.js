232a5a0369a7e014a49842494119ab232da12c1c
jorge.burgos <mtnburgos@yahoo.es>
2013-06-13 00:10:37 -0400

    Incorporate the edit todo functionality.
    When the user double clicks over the todo item, it shows a textbox with
    the title inside it. The todo is updated when the user hits enter or
    clicks out of the textbox.

--- WebContent/todo/js/app/views/content.view.js
+++ WebContent/todo/js/app/views/content.view.js
@@ -7,6 +7,16 @@ define([
         tagName: 'li',
         className: 'viewing',
         
+        events: {
+            'dblclick .view': 'edit',
+            'blur .edit': 'saveAndClose',
+            'keypress .edit': 'updateOnEnter',
+        },
+        
+        ui: {
+            input: 'input[type="text"]',
+        },
+        
         initialize: function() {
             this.model.on("change", this.render, this);
         },
@@ -17,6 +27,25 @@ define([
             } else {
                 this.$el.removeClass('done');
             }
+        },
+        
+        edit: function() {
+            this.$el.addClass('editing');
+            
+            this.ui.input[0].selectionStart = 0;
+            this.ui.input[0].selectionEnd = this.ui.input.val().length;
+            this.ui.input.focus();
+        },
+        
+        saveAndClose: function() {
+            var title = this.ui.input.val();
+            this.model.save({title: title});
+            this.$el.removeClass('editing');
+        },
+        
+        updateOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            this.saveAndClose();
         },
         
        
