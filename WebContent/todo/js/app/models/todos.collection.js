b50b8616bf249577af552758ff3fd9f3a23430c5
jorge.burgos <mtnburgos@yahoo.es>
2013-06-15 10:08:38 -0400

    complete add and mark all functionality

--- WebContent/todo/js/app/models/todos.collection.js
+++ WebContent/todo/js/app/models/todos.collection.js
@@ -5,6 +5,17 @@ define([
     var TodosCollection = Backbone.Collection.extend({
         model: TodoModel,
         localStorage: new Backbone.LocalStorage("todos-backbone"),
+        
+        addNewModel: function(title) {
+            var ordered = this.size();
+            var todoModel = new TodoModel({title: title, ordered: ordered});
+            this.add(todoModel);
+        },
+        
+        markAll: function(done) {
+            this.each(function(todo){ todo.save({done: done}); });
+        },
+        
     });
     
     return TodosCollection;
--- WebContent/todo/js/app/views/content.views.js
+++ WebContent/todo/js/app/views/content.views.js
@@ -6,7 +6,34 @@ define([
     var ContentViews = Backbone.Marionette.CompositeView.extend({
         template: _.template(ContentViewsTemplate),
         itemView: ContentView,
-        itemViewContainer: '#todo-list'
+        itemViewContainer: '#todo-list',
+        
+        initialize: function(){
+            _.bindAll(this,"createOnEnter");
+        },
+        
+        events: {
+            "keypress #new-todo": "createOnEnter",
+            "click #toggle-all": "markAll"
+        },
+        
+        ui: {
+            title: '#new-todo',
+            allCheckbox: "#toggle-all"
+        },
+        
+        createOnEnter: function(e) {
+            if (e.keyCode != 13) return;
+            
+            var title = $.trim(this.ui.title.val());
+            this.collection.addNewModel(title);
+            this.ui.title.val("");
+        },
+        
+        markAll: function() {
+            var done = this.ui.allCheckbox[0].checked;
+            this.collection.markAll(done);
+        },
         
     });
     
